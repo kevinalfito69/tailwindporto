3b52ead49f93da667e82b159aac91f42ec969aeb
kevinalfito69 <kevinalfito69@gmail.com>
2022-07-25 21:20:34 +0700

    add to top button

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,37 @@
-// nav fix 
-window.onscroll = ()=>{
-    const header = document.querySelector('header')
-    const fixnav= header.offsetTop;
-    if(window.pageYOffset>fixnav){
-        header.classList.add('navbar-fix')
-    }else{
-        header.classList.remove('navbar-fix')
-    }
-}
-//Hamburger Menu
-const hamburger = document.getElementById('hamburger')
-// navmenu
-const navMenu = document.getElementById('nav-menu')
+// nav fix
+window.onscroll = () => {
+  const header = document.querySelector("header");
+  const backtop = document.getElementById("backtop");
+  const fixnav = header.offsetTop;
+  if (window.pageYOffset > fixnav) {
+    header.classList.add("navbar-fix");
 
-hamburger.addEventListener('click',()=>{
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
+    backtop.classList.remove("hidden");
+  } else {
+    header.classList.remove("navbar-fix");
+    backtop.classList.add("hidden");
+  }
+};
+//Hamburger Menu
+const hamburger = document.getElementById("hamburger");
+// navmenu
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
+
+// klik di luar nav menu
+window.addEventListener("click", (event) => {
+    if (
+        event.target.matches("#hamburger") ||
+        event.target.matches("#nav-menu")
+
+    ) {
+        return;
+    }
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+    }
+);
